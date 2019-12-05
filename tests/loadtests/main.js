a03443750e131c5a95709e066e99824946d01e2a
Robin Gustafsson <i@robingustafsson.com>
2019-12-05 11:06:39 +0100

    Change threshold

--- tests/loadtests/main.js
+++ tests/loadtests/main.js
@@ -17,7 +17,7 @@ export let options = {
         {target: 0, duration: '15s'}
     ],
     thresholds: {
-        "http_req_duration": ["p(95)<300"]
+        "http_req_duration": ["p(95)<500"]
     },
     ext: {
         loadimpact: {
